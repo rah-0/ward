848791a47882ae43ea69b1cb0f0db66e817166e3
Robert.Artenie <robert.artenie@aelno.com>
2026-05-13 18:40:29 +0000

    FMT

--- types/strs/regexp.go
+++ types/strs/regexp.go
@@ -3,12 +3,12 @@ package strs
 import "regexp"
 
 var (
-	RegexpSha512           = regexp.MustCompile(`^[a-fA-F0-9]{128}$`)
-	RegexpHasLowercase     = regexp.MustCompile(`[a-z]`)
-	RegexpHasUppercase     = regexp.MustCompile(`[A-Z]`)
-	RegexpDigitsOnly       = regexp.MustCompile(`^\d+$`)
-	RegexpHasDigit         = regexp.MustCompile(`\d`)
-	RegexpNonNegativeInt   = regexp.MustCompile(`^(0|[1-9]\d{0,8})$`)
-	RegexpUsernameChars    = regexp.MustCompile(`^[a-zA-Z0-9_.@-]+$`)
-	RegexpHasSpecialChar   = regexp.MustCompile(`[@$!%*?&_\-=]`)
+	RegexpSha512         = regexp.MustCompile(`^[a-fA-F0-9]{128}$`)
+	RegexpHasLowercase   = regexp.MustCompile(`[a-z]`)
+	RegexpHasUppercase   = regexp.MustCompile(`[A-Z]`)
+	RegexpDigitsOnly     = regexp.MustCompile(`^\d+$`)
+	RegexpHasDigit       = regexp.MustCompile(`\d`)
+	RegexpNonNegativeInt = regexp.MustCompile(`^(0|[1-9]\d{0,8})$`)
+	RegexpUsernameChars  = regexp.MustCompile(`^[a-zA-Z0-9_.@-]+$`)
+	RegexpHasSpecialChar = regexp.MustCompile(`[@$!%*?&_\-=]`)
 )
